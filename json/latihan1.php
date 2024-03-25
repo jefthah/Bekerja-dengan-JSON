1d0812ae7efd15bef40130753262142ffd659cdc
Jefta Supraja <94596281+jefthah@users.noreply.github.com>
2024-03-25 20:57:30 +0700

    initial commit

---
+++ json/latihan1.php
@@ -0,0 +1,19 @@
+<?php 
+$mahhaasiswa = [
+    [
+        "nama" => "Jefta Supraja",
+        "nim" => "2110511131",
+        "email" => "jefta.supraja@gmail.com"
+    ],
+    [
+        "nama" => "Forsaken",
+        "nim" => "2110511132",
+        "email" => "forsaken.supraja@gmail.com"
+    ]
+
+];
+
+$data = json_encode($mahhaasiswa);
+echo $data;
+
+?>
